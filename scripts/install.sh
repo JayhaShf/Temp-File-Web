c1a947b459ea95d4cc712ab538b1ba436ee08f54
root <root@Jayha-HK>
2026-05-03 00:30:54 +0800

    修复上传认证文件权限导致的 500 错误

--- scripts/install.sh
+++ scripts/install.sh
@@ -659,6 +659,18 @@ random_password() {
   openssl rand -base64 18 | tr -d '=+/\n' | cut -c1-20
 }
 
+set_auth_file_permissions() {
+  local owner_group
+
+  chmod 0640 "$SITE_DIR/file-upload.htpasswd"
+  if id -u "$TFW_USER" >/dev/null 2>&1; then
+    owner_group="$(id -gn "$TFW_USER")"
+    chown root:"$owner_group" "$SITE_DIR/file-upload.htpasswd"
+  else
+    chown root:root "$SITE_DIR/file-upload.htpasswd"
+  fi
+}
+
 render_template() {
   local src="$1"
   local dest="$2"
@@ -885,6 +897,7 @@ write_auth_file() {
   umask 077
   printf '%s:%s\n' "$AUTH_USER" "$hash" > "$SITE_DIR/file-upload.htpasswd"
   umask "$old_umask"
+  set_auth_file_permissions
   AUTH_PASSWORD="$password"
 }
 
